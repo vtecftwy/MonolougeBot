9df582ad9e7fb2ae8700eae35009c5328e42523f
pbarrett520 <104513707+pbarrett520@users.noreply.github.com>
2023-05-09 21:55:34 +0800

    Add files via upload
    The UI has makeup now

--- GPT_NEO_Generator_UI_1.0.py
+++ GPT_NEO_Generator_UI_1.0.py
@@ -1,14 +1,47 @@
 from turtle import color
 from transformers import pipeline
+from diffusers import __version__
 import tkinter.messagebox
+from PIL import Image, ImageTk
+from fastcore.all import concat
+import torch, logging
+from pathlib import Path
+from huggingface_hub import notebook_login
+from diffusers import StableDiffusionPipeline
+from PIL import Image
 from tkinter import filedialog as fd
 from tkinter import ttk
 from tkinter.messagebox import askyesno
 from tkinter import *
 from tkinter import font
+from random import choice
 
+torch.manual_seed(1)
+if not (Path.home()/'.huggingface'/'token').exists(): notebook_login()
+
+model_string_v1 = 'CompVis/stable-diffusion-v1-4'
+model_string_v2 = 'stabilityai/stable-diffusion-2-1-base'
+pipe = StableDiffusionPipeline.from_pretrained(model_string_v2, revision="fp16", torch_dtype=torch.float16).to("cuda")
+
+art_styles = ["anime", "oil painting", "drawing","water color painting" "cartoon", "digital illustration", "disney animation studios", "pixar 3-d animation"]
+genres = ["steampunk", "sciene fiction", "film noir", "fantasy", "90s", "80s", "70s", "60s", "50s", "western"]
+pallettes = ["cool colors", "warm colors", "sepia", "black and white", "cell shaded"]
+misc_parameters = ["rule of threes", "golden ratio", "4k resolution", "trending on deviantart"]
+
+def image_gen(prompt):
+    pipe.enable_attention_slicing()
+    results = pipe(prompt= f"{choice(misc_parameters)}, prompt , {choice(art_styles)}, {choice(genres)}, {pallettes}, {choice(misc_parameters)}", 
+               height=None, width=None, num_inference_steps=50, guidance_scale=7.5, 
+               num_images_per_prompt=1, output_type='pil', return_dict=True, callback=None,
+               negative_prompt= """mutilated fingers, twisted fingers, deformed fingers, racism, sexism, homophobia, china, chinese, communism,
+               communist party, xi jinping, mao zedong, han chinese, taiwan, kmt, guomindang, kuomintang, penis, vagina, human genetalia, sex, murder, rape,
+               tibet, pixellated, pixellation, blur, blurry, static, non-objective, abstract, tian'an men square, hong kong, protest, covid, covid-19,
+               xinjiang, uyghur, uighur, muslim, islam, islamic, slaves, slavery, israel, jews, holocaust""")
+    
+    return results.images
 
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
+
 #=====================================================
 def clear_prompt():
     output_field.delete(1.0,"end")
@@ -22,7 +55,7 @@ def process_text():
     print(prompt)
     '''prompt = input("What is on your mind?: ")'''
     
-    result = generator(prompt,min_length=200, max_length=210, do_sample=True, temperature=0.89)
+    result = generator(prompt,min_length=175, max_length=225 , do_sample=True, temperature=0.89)
     raw_result = (result[0]['generated_text'])
     
     ##### Generate indexes for sentence ending punctuation #####
